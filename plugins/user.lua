f111c448d39e99e6e76a7e8c9d563aa1662ef6cb
jun yamaguchi <jun.yamaguchi@arch-dx.com>
2023-09-21 21:58:21 +0900

    first

---
+++ plugins/user.lua
@@ -0,0 +1,14 @@
+return {
+    "github/copilot.vim",
+    lazy=false,
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
